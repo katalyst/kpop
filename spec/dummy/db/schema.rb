096d3772d0e285a10705574ef13daa2256a8b62c
Stephen Nelson <stephen@katalyst.com.au>
2024-07-12 12:59:23 +0930

    Update and apply rubocop

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -20,5 +22,4 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_26_055211) do
   create_table "parents", force: :cascade do |t|
     t.string "name"
   end
-
 end
